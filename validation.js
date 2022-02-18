5fd321ef68bab5c304e9cf74b72525ea67586bb0
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2022-02-17 19:34:36 -0600

    redex practice - matching a username

--- validation.js
+++ validation.js
@@ -4,8 +4,14 @@ const inputs = document.querySelectorAll('input');
 
 
 const patterns = {
-    telephone: /^\d{11}$/
+    telephone: /^\d{11}$/,
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/
 };
+
+// metacharacter \w - match any letter uppercase or lower case, any digit from 0 to 9 and underscores.
+
+// \w@- match any word character, any @ symbol or hypen.
 
 // validation function
 function validate(field, regex) {
